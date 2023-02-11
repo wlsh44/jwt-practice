cbde9eb43af51e7837be5b2850310424da6420f4
wlsh44 <k87913j@gmail.com>
2023-02-12 00:19:19 +0900

    fix: 인증 헤더 공백 기준으로 나누도록 수정

--- src/main/java/com/example/jwtpractice/controller/AuthorizationExtractor.java
+++ src/main/java/com/example/jwtpractice/controller/AuthorizationExtractor.java
@@ -2,21 +2,25 @@ package com.example.jwtpractice.controller;
 
 import com.example.jwtpractice.exception.AuthExtractException;
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.HttpHeaders;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class AuthorizationExtractor {
 
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String TOKEN_TYPE = "Bearer ";
+    private static final String TOKEN_TYPE = "Bearer";
 
     public static String extract(HttpServletRequest request) {
-        String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
+        return getToken(authHeader);
+    }
+
+    private static String getToken(String authHeader) {
         validateAuthHeaderExists(authHeader);
-        validateStartWithTokenType(authHeader);
-
-        return authHeader.substring(TOKEN_TYPE.length());
+        String[] authContents = authHeader.split(" ");
+        validateStartWithTokenType(authContents);
+        return authContents[1];
     }
 
     private static void validateAuthHeaderExists(String authHeader) {
@@ -25,8 +29,8 @@ public class AuthorizationExtractor {
         }
     }
 
-    private static void validateStartWithTokenType(String authHeader) {
-        if (!authHeader.startsWith(TOKEN_TYPE)) {
+    private static void validateStartWithTokenType(String[] authContents) {
+        if (!(authContents.length == 2 && TOKEN_TYPE.equals(authContents[0]))) {
             throw new AuthExtractException();
         }
     }
